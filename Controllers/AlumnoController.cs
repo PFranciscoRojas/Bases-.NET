cd69f9790b2b0e615525320e29a4c6b7d77f28f5
Francisco Rojas <franciscorojas92@gmail.com>
2019-09-11 12:57:19 -0500

    subiendo archivos faltantes

---
+++ Controllers/AlumnoController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp_.Net.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_.Net.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        private EscuelaContext _context;
+
+        public AlumnoController(EscuelaContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: Alumno
+        public ActionResult Index()
+        {
+            return View(_context.Alumnos.FirstOrDefault()); 
+        }
+
+
+        public ActionResult MultiAlumno()
+        {
+            ViewBag.Fecha = DateTime.Now;
+
+            return View("MultiAlumnos", _context.Alumnos);
+        }
+
+        // GET: Alumno/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Alumno/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alumno/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumno/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumno/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alumno/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Alumno/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/AsignaturaController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp_.Net.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_.Net.Controllers
+{
+    public class AsignaturaController : Controller
+    {
+
+        private EscuelaContext _context;
+        public AsignaturaController(EscuelaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Asignatura
+        public ActionResult Index()
+        {
+            return View(_context.Asignaturas.FirstOrDefault());
+        }
+
+
+        public ActionResult MultiAsignatura()
+        {
+            ViewBag.Fecha = DateTime.Now;
+            return View("MultiAsignaturas", _context.Asignaturas);
+        }
+
+        // GET: Asignatura/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Asignatura/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asignatura/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asignatura/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Asignatura/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Asignatura/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Asignatura/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
